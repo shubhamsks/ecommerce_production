f9ea135b75ffb13cecc1851544d799d620c2aadc
shubhamsks <shubhamkumar7051@gmail.com>
2019-10-13 17:11:27 +0530

    Added django_heroku

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,4 +133,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,"static", "static_files"),)
 
 STRIPE_SECRET_KEY = "sk_test_Ef3uQr0mMNXBS9Z0E2u0OCbu00S9xCRdTv"
 STRIPE_PUBLISHABLE_KEY = "pk_test_egX0tzLTlo6Csig6Pauay4LJ00HEFUUYq0"
+django_heroku.settings(locals())
 
