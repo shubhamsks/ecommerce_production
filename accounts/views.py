22a587da426d0794dc3990d73e8456839db72ee9
shubhamsks <shubhamkumar7051@gmail.com>
2019-10-14 16:54:32 +0530

    made some changes in UI for better experience

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth import logout, login, authenticate
 from .forms import LoginForm, RegistrationForm
 from django.urls import reverse
+from django.contrib import messages
 def logout_view(request):
+    messages.info(request,"You have been logged out successfully.")
     logout(request)
     return HttpResponseRedirect("/")
 
@@ -14,6 +16,8 @@ def login_view(request):
         password = form.cleaned_data["password"]
         user = authenticate(username=username, password=password)
         login(request, user)
+        messages.success(request,"Yoh have been logged in successfully.")
+        return HttpResponseRedirect(reverse('home'))
 
     context = {
         "form": form,
@@ -31,6 +35,7 @@ def registration_view(request):
         password = form.cleaned_data.get('password2')
         user = authenticate(request,username = username, password = password)
         login(request, new_user)
+        messages.success(request,"You have been registered successfully.")
         return HttpResponseRedirect(reverse('home'))
     #     # username = form.cleaned_data["username"]
     #     # password = form.cleaned_data["password"]
